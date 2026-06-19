1e9b846de0ff67a7e97d6988ca84f2eb2eccf11f
abhinand-psq <abhinandpsq2@gmail.com>
2026-06-19 14:59:17 +0530

    sdf

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,7 +1,7 @@
 const { add, subtract, multiply } = require('../utils/math');
 
 describe('Math Utility Unit Tests', () => {
-  
+
   // Test Case 1: Testing addition
   test('should correctly add two numbers', () => {
     const result = add(2, 3);
@@ -15,7 +15,7 @@ describe('Math Utility Unit Tests', () => {
   });
 
   // Test Case 3: Testing multiplication
-  test('should correctly multiply two numbers', () => {
+  test('should correctly multiply two numbers hoooo wanot', () => {
     const result = multiply(4, 3);
     expect(result).toBe(12);
   });
